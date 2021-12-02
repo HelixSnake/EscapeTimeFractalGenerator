bd7f7284dfcadca67835441cbef65dc6a34b86ff
LandonTownsend <landon.townsend@gmail.com>
2021-12-02 09:20:38 -0800

    Added unit test for FractalCommandList array construction

--- EscapeTimeFractalGenerator/EscapeTimeFractalGenerator/test.cpp
+++ EscapeTimeFractalGenerator/EscapeTimeFractalGenerator/test.cpp
@@ -1,4 +1,5 @@
 #include "test.h"
+#include "FractalCommandList.h"
 
 // Function to add two integers 
 int addIntegers(const int& a, const int& b)
@@ -12,8 +13,19 @@ BEGIN_TEST(AddProps, Commutative)
 	EXPECT_EQ(addIntegers(3, 5), addIntegers(5, 3))
 }END_TEST
 
+BEGIN_TEST(FractalCommandList, ArrayConstruction)
+	{
+		unsigned int ints[5] = { 2, 4, 5, 7, 8 };
+		FractalCommandList fcl(0, 0, 5, ints);
+		for (int i = 0; i < 5; i++)
+		{
+			EXPECT_EQ(ints[i], fcl.GetCommandList()[i])
+		}
+	}END_TEST
+
 int RunAllUnitTests()
 	{
 		RUN_TEST(AddProps, Commutative)
+			RUN_TEST(FractalCommandList, ArrayConstruction)
 			return 0;
 	}
